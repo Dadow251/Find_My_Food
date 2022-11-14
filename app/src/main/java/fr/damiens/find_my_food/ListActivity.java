9b7184a37568558880888f26f92de5d530d559e7
Dadow251 <damien.leroux@edu.esiee.fr>
2022-11-14 17:12:09 +0100

    Activities de base (Main & List)

---
+++ app/src/main/java/fr/damiens/find_my_food/ListActivity.java
@@ -0,0 +1,14 @@
+package fr.damiens.find_my_food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
--- app/src/main/java/fr/damiens/find_my_food/MainActivity.java
+++ app/src/main/java/fr/damiens/find_my_food/MainActivity.java
@@ -2,7 +2,10 @@ package fr.damiens.find_my_food;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Appelée lors d'un appui sur le bouton searchButton */
+    public void search(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, ListActivity.class);
+        /* Utilisation du texte contenu dans la searchBar
+        EditText editText = (EditText) findViewById(R.id.searchBar);
+        String message = editText.getText().toString();
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        */
+        startActivity(intent);
+
     }
 }
