f0b115e6c4906634275be423ba5465ee8643c08b
Damien <damien.leger@edu.esiee.fr>
2022-11-15 16:45:29 +0100

    Ajout : - classe DatabaseDownloader.java         - implémentations de firebase dans build.gradle(Find_My_Food)

--- DatabaseDownloader.java
+++ DatabaseDownloader.java
@@ -1,8 +1,3 @@
-import com.google.firebase.firestore.FirebaseFirestore;
-
 public class DatabaseDownloader {
-
-    // Access a Cloud Firestore instance from your Activity
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 }
