9b2c6fc83b71528a30325b6cf63c0c043dece1bb
Dadow251 <damien.leroux@edu.esiee.fr>
2022-11-22 11:10:26 +0100

    Ajout : - Recherche - Correction du problème de duplication des items de la RV

--- app/src/main/java/fr/damiens/find_my_food/ListActivity.java
+++ app/src/main/java/fr/damiens/find_my_food/ListActivity.java
@@ -12,7 +12,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,7 +20,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -29,20 +27,6 @@ public class ListActivity extends AppCompatActivity {
     private ArrayList<FoodItem> data;
     private RVAdapter rvAdapter;
     private TextView textView;
-
-    void initData() { // crée des données factices pour évaluer l'affichage
-        data = new ArrayList<>();
-
-        data.add(new FoodItem("Tomate grappe", 1.20, "Carrefour"));
-        data.add(new FoodItem("Tomate cerise", 1.15, "Auchan"));
-        data.add(new FoodItem("Tomate allongée", 1.35, "Super U"));
-        data.add(new FoodItem("Tomate coeur de boeuf", 1.38, "E. Leclerc"));
-        data.add(new FoodItem("Tomate verte", 1.07, "Intermarché"));
-        data.add(new FoodItem("Tomate noire", 1.42, "Lidl"));
-        data.add(new FoodItem("Tomate cerise allongée", 0.96, "Grand frais"));
-        data.add(new FoodItem("Tomate ronde", 1.03, "Auchan"));
-        data.add(new FoodItem("Tomate jaune", 1.12, "Super U"));
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,34 +43,42 @@ public class ListActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
-//        ArrayList<String> stringList = new ArrayList<>();
-
         data = new ArrayList<>();
-//        initData(); // données factices
 
         rvAdapter = new RVAdapter(this, data);
         recyclerView.setAdapter(rvAdapter);
 
         // Message de recherche
         Intent intent = getIntent();
-        String message = intent.getStringExtra(Intent.EXTRA_TEXT);
-        String resultDisplay = "Résultats pour " + message;
+        String keyWords = intent.getStringExtra(Intent.EXTRA_TEXT);
+        String resultDisplay = "Résultats pour " + keyWords;
         textView.setText(resultDisplay);
 
         // Récupération des données de Realtime database (Firebase)
         dbRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Toast.makeText(ListActivity.this, "onDataChange", Toast.LENGTH_SHORT).show();
+                data.clear();
+                String[] keyWordsSearch = keyWords.toLowerCase().split(" ");
                 for(DataSnapshot article : snapshot.getChildren()){
                     String description = article.child("description").getValue(String.class);
                     double price = article.child("price").getValue(double.class);
                     String market = article.child("market").getValue(String.class);
                     FoodItem foodItem = new FoodItem(description, price, market);
 
-                    data.add(foodItem);
-                    if(foodItem != null)
-                        Toast.makeText(ListActivity.this, foodItem.toString(), Toast.LENGTH_SHORT).show();
+                    if(description != null) {
+                        String[] keyWordsResult = description.toLowerCase().split(" ");
+                        int nbSim = 0; // nombre de mots similaires
+                        int nbWords = keyWordsSearch.length;
+                        for (String s : keyWordsSearch) {
+                            for (String r : keyWordsResult) {
+                                if (s.equals(r))
+                                    nbSim++;
+                            }
+                        }
+                        if(nbSim > 0) // sensibilité de la reconnaissance
+                            data.add(foodItem); // W : liste vide en dehors de la classe
+                    }
                 }
                 rvAdapter.notifyDataSetChanged();
             }
