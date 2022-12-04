84a5623b9a2eaa3b8b7c4932dfa888b26e7c55f2
Damien <damien.leger@edu.esiee.fr>
2022-12-04 18:05:04 +0100

    Modification : - Classe AddFoodItemActivity.java

--- app/src/main/java/fr/damiens/find_my_food/AddFoodItemActivity.java
+++ app/src/main/java/fr/damiens/find_my_food/AddFoodItemActivity.java
@@ -3,11 +3,14 @@ package fr.damiens.find_my_food;
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,8 +21,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class AddFoodItemActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.Locale;
 
+public class AddFoodItemActivity extends AppCompatActivity {
+    // nom de l'aliment
+    public static String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,7 @@ public class AddFoodItemActivity extends AppCompatActivity {
         EditText editMarket = findViewById(R.id.editTextMarket);
         EditText editURL = findViewById(R.id.editTextURL);
 
+
         // Récupération des informations de l'article à ajouter
         String aliment = editAliment.getText().toString();
         String description = editDescription.getText().toString();
@@ -47,61 +55,89 @@ public class AddFoodItemActivity extends AppCompatActivity {
         String market = editMarket.getText().toString();
         String url = editURL.getText().toString();
 
-        // Read from the database
+        // Lecture et écriture dans la base de données
         dbRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
+
                 String[] keyWordsSearch = description.toLowerCase().split(" ");
                 boolean article_exists = false;
-                String name ="";
+                name =aliment+"_0";
+
                 for(DataSnapshot article : dataSnapshot.getChildren()) {
                     String value = article.child("market").getValue(String.class);
                     String[] keyWordsResult = article.child("description").getValue(String.class).toLowerCase().split(" ");
+                    int mots_desc = 0;
                     if(value.toLowerCase().equals(market.toLowerCase())){
                         for (String s : keyWordsSearch) {
                             for (String r : keyWordsResult) {
                                 if (s.equals(r)) {
-                                    article_exists = true;
-                                    name = article.getKey();
-                                    break;
+                                    mots_desc += 1;
                                 }
                             }
                         }
+                        if(mots_desc == keyWordsResult.length){
+                            article_exists = true;
+                           name = article.getKey();
+                        }
+                    }
+                    // Atribution numero d'identification du nouvel aliment à partir des autres aliment de la base de données
+                    if(!article_exists) {
+                        if (article.getKey().toLowerCase().contains(aliment.toLowerCase())) {
+                            int num_result = (int) article.getKey().toLowerCase().charAt(article.getKey().toLowerCase().length() - 1);
+                            int num_add = (int) name.toLowerCase().charAt(name.toLowerCase().length() - 1);
+                            //int num_result = Integer.parseInt(article.getKey().toLowerCase().split("_")[1]);
+                            //int num_add = Integer.parseInt(name.toLowerCase().split("_")[1]);
+                            if (num_add <= num_result) {
+                                num_add = num_result + 1;
+                            }
+                            name = aliment + "_" + (char) num_add;
+                        }
                     }
                 }
-                // Ecriture dans la base de données
+
+                // Création de l'aliment dans la base de donnée
                 if(!article_exists){
-                    dbRef.child(aliment).child("description").setValue(description);
-                    dbRef.child(aliment).child("price").setValue(price);
-                    dbRef.child(aliment).child("market").setValue(market);
-                    if(url.isEmpty()) {
-                        dbRef.child(aliment).child("url").setValue("https://www.icone-png.com/png/54/53892.png");
-                    }
-                    else{
-                        dbRef.child(aliment).child("url").setValue(url);
-                    }
-                    // Retour à l'Activity Main et affichage du message de bon déroulement de l'ajout
+                    dbRef.child(name).child("description").setValue(description);
+                    dbRef.child(name).child("price").setValue(price);
+                    dbRef.child(name).child("market").setValue(market);
+                    dbRef.child(name).child("url").setValue(url);
 
-                    String message = aliment + " a bien été ajouté à la base de données";
+                    // Retour à l'Activity Main et affichage du message de bon déroulement de l'ajout
+                    String message = name + " a bien été ajouté à la base de données";
                     Intent intent = new Intent(AddFoodItemActivity.this, MainActivity.class);
                     intent.putExtra(Intent.EXTRA_TEXT, message);
                     startActivity(intent);
                 }
+                // Demande de modification de l'aliment dans la base de donnée à l'utilisateur
                 else{
-                    Toast.makeText(AddFoodItemActivity.this,"Article "+name+" existe déjà", Toast.LENGTH_SHORT).show();
-                    if(!price.isNaN()) {
-                        dbRef.child(name).child("price").setValue(price);
-                    }
-                    if(!url.isEmpty()) {
-                        dbRef.child(name).child("url").setValue(url);
-                    }
+                    AlertDialog.Builder builder = new AlertDialog.Builder(AddFoodItemActivity.this);
+                    builder.setMessage("Cet aliment existe déjà dans la base de données. Voulez-vous modifier cet aliment?")
+                            .setPositiveButton("Oui je veux", new DialogInterface.OnClickListener() {
+                                // Modification du prix et de l'image de l'aliment
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    if(!price.isNaN()) {
+                                        dbRef.child(name).child("price").setValue(price);
+                                    }
+                                    if(!url.isEmpty()) {
+                                        dbRef.child(name).child("url").setValue(url);
+                                    }
 
-                    String message = name + " a bien été modifié dans la base de données";
-                    Intent intent = new Intent(AddFoodItemActivity.this, MainActivity.class);
-                    intent.putExtra(Intent.EXTRA_TEXT, message);
-                    startActivity(intent);
+                                    // Retour à l'Activity Main et affichage du message de bon déroulement de l'ajout
+                                    String message = name + " a bien été modifié dans la base de données";
+                                    Intent intent = new Intent(AddFoodItemActivity.this, MainActivity.class);
+                                    intent.putExtra(Intent.EXTRA_TEXT, message);
+                                    startActivity(intent);
+                                }
+                            })
+                            .setNegativeButton("Non je ne veux pas", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Toast.makeText(AddFoodItemActivity.this,"Base de données non modifiée", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                    builder.create().show();
                 }
             }
             @Override
